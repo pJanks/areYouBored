b331bfbce2aa6d9daba744ffd922e139c9e6b041
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-15 21:31:40 -0800

    Get random activity for use on home screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { Header } from './components/Header/Header'
+import './App.css'
 
 const StyledAppWrapper = styled.div`
   color: blue;
   font-size: 300%;
 `
 
-const App = () => {
+
+const App =  () => {
+  const [activity, setActivity] = useState({})
+  
+  useEffect(() => {
+    fetch('http://www.boredapi.com/api/activity')
+      .then(response => response.json())
+      .then(json => setActivity(json))
+  }, [])
+
   return (
     <StyledAppWrapper>
       <Header />
