593a721aaa5161c5f7e3a21ee96c20a9156ba76d
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-21 13:41:17 -0800

    Add favorites page functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import styled from 'styled-components'
 import { useEffect, useState } from 'react'
 import { Header } from './components/Header/Header'
 import { ActivityCard } from './components/ActivityCard/ActivityCard'
 import './App.css'
 
+const StyledErrorMessage = styled.span`
+  background-color: inherit;
+  color: red;
+  font-size: 160%;
+  font-weight: 700;
+  margin: 60px;
+`
+
 const App =  () => {
   const [activity, setActivity] = useState({})
   const [isAboutInfoActive, setIsAboutInfoActive] = useState(false)
+  const [isViewFavoritesActive, setIsViewFavoritesActive] = useState(false)
+  const [error, setError] = useState('')
 
   useEffect(() => {
     fetch('http://www.boredapi.com/api/activity')
+    // fetch('http://www.boredapi.com/api/activity?key=3621244')
       .then(response => response.json())
       .then(json => setActivity(json))
-      .catch(err => console.log(err))
-  }, [])
+      .catch(err => setError(err))
+  }, [error])
 
   return (
     <div>
-      <Header setIsAboutInfoActive={setIsAboutInfoActive} />
-      <ActivityCard currentActivity={activity} setActivity={setActivity} isAboutInfoActive={isAboutInfoActive} setIsAboutInfoActive={setIsAboutInfoActive} />
+      <Header isViewFavoritesActive={isViewFavoritesActive} setIsViewFavoritesActive={setIsViewFavoritesActive} setIsAboutInfoActive={setIsAboutInfoActive} />
+      {error ? (
+        <StyledErrorMessage>There was an error: {error}</StyledErrorMessage>
+      ) : (
+        <ActivityCard currentActivity={activity} setActivity={setActivity} isAboutInfoActive={isAboutInfoActive} setIsAboutInfoActive={setIsAboutInfoActive} isViewFavoritesActive={isViewFavoritesActive} setIsViewFavoritesActive={setIsViewFavoritesActive} />
+      )}
     </div>
   )
 }
