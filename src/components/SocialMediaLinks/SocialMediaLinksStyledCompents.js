a79d7ebc8cbf25803804867f27a23cfb16c045cd
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-21 14:40:42 -0800

    Adjust styles

--- src/components/SocialMediaLinks/SocialMediaLinksStyledCompents.js
+++ src/components/SocialMediaLinks/SocialMediaLinksStyledCompents.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
 
 export const StyledIcon = styled.img`
+  border-radius: 100px;
+  box-shadow: 0 2px 10px 0 #FFF;
   height: 40px;
   width: 40px;
 
   &:hover {
+    box-shadow: 0 8px 20px 0 #FFF;
     height: 45px;
     width: 45px;
   }
