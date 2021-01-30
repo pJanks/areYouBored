c787e404e925481ee526ebdeb42b5f39614926d1
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2021-01-30 11:55:59 -0800

    Fix bug with social media links

--- src/components/SocialMediaLinks/SocialMediaLinks.js
+++ src/components/SocialMediaLinks/SocialMediaLinks.js
@@ -1,14 +1,20 @@
 import gitHubIcon from '../../images/github_icon.png'
 import twitterIcon from '../../images/twitter_icon.png'
 import linkedInIcon from '../../images/linkedin_icon.png'
-import { StyledIconWrapper, StyledIcon } from './SocialMediaLinksStyledCompents'
+import { StyledIconWrapper, StyledIconsWrapper, StyledIcon } from './SocialMediaLinksStyledCompents'
 
 export const SocialMediaLinks = () => {
   return(
-    <StyledIconWrapper>
-      <a rel='noopener noreferrer' aria-label='my github profile' href='https://github.com/pJanks' target='_blank'><StyledIcon alt='Github icon' src={gitHubIcon} /></a>
-      <a rel='noopener noreferrer' aria-label='my linkedin profile' href='https://linkedin.com/in/johnnykcassidy' target='_blank'><StyledIcon alt='Linkedin icon' src={linkedInIcon} /></a>
-      <a rel='noopener noreferrer' aria-label='my twitter profile' href='https://twitter.com/pJanksio' target='_blank'><StyledIcon alt='Twitter icon' src={twitterIcon} /></a>
-    </StyledIconWrapper>
+    <StyledIconsWrapper>
+      <StyledIconWrapper>
+        <a rel='noopener noreferrer' aria-label='my github profile' href='https://github.com/pJanks' target='_blank'><StyledIcon alt='Github icon' src={gitHubIcon} /></a>
+      </StyledIconWrapper>
+      <StyledIconWrapper>
+        <a rel='noopener noreferrer' aria-label='my linkedin profile' href='https://linkedin.com/in/johnnykcassidy' target='_blank'><StyledIcon alt='Linkedin icon' src={linkedInIcon} /></a>
+      </StyledIconWrapper>
+      <StyledIconWrapper>
+        <a rel='noopener noreferrer' aria-label='my twitter profile' href='https://twitter.com/pJanksio' target='_blank'><StyledIcon alt='Twitter icon' src={twitterIcon} /></a>
+      </StyledIconWrapper>
+    </StyledIconsWrapper>
   )
 }
